df34e9a25a897aaa3395511976ea483b65b9f2d5
gracebir <grace.devolop@gmail.com>
2023-06-18 18:05:29 +0200

    service components

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Service from './Service'
+import { services } from '@/util/dummy'
 
 function Services() {
   return (
-    <div>
-      services
+    <div className='max-w-5xl mx-auto grid  grid-cols-1 lg:grid-cols-2 gap-24'>
+      {services.map(({id, title, desc, img}, _)=> (
+        <Service key={id} title={title} desc={desc} img={img}/>
+      ))}
     </div>
   )
 }
