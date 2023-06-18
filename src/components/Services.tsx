3df74501bf69ac23cb283af9847b307d1e24085c
gracebir <grace.devolop@gmail.com>
2023-06-18 18:52:27 +0200

    reducing gaps

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,7 +4,7 @@ import { services } from '@/util/dummy'
 
 function Services() {
   return (
-    <div className='max-w-5xl mx-auto grid  grid-cols-1 lg:grid-cols-2 gap-24'>
+    <div className='max-w-5xl mx-auto grid  grid-cols-1 lg:grid-cols-2 gap-16 px-4 lg:px-0 py-20 lg:gap-24'>
       {services.map(({ id, title, desc, img }, _) => (
         <Service key={id} title={title} desc={desc} img={img} />
       ))}
