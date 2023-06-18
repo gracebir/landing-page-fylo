cb379363371582ed4983f1641f71469109a38917
gracebir <grace.devolop@gmail.com>
2023-06-18 23:16:46 +0200

    testimonial to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Product from '@/components/Product'
 import Services from '@/components/Services'
+import Testimonials from '@/components/Testimonials'
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
    <main className="w-full bg-main-bg min-h-screen">
     <Services/>
     <Product/>
+    <Testimonials/>
    </main>
    </>
   )
