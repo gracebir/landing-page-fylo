08068608de7e637456e93c9b3e5de5ec6986d471
gracebir <grace.devolop@gmail.com>
2023-06-19 23:46:15 +0200

    adding call to action components and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Action from '@/components/Action'
+import CallAction from '@/components/CallAction'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Product from '@/components/Product'
@@ -8,15 +10,19 @@ import Testimonials from '@/components/Testimonials'
 
 export default function Home() {
   return (
-   <>
-   <Header/>
-   <Hero/>
-   <Action/>
-   <main className="w-full bg-main-bg min-h-screen">
-    <Services/>
-    <Product/>
-    <Testimonials/>
-   </main>
-   </>
+    <>
+      <Header />
+      <Hero />
+      <Action />
+      <main className="w-full bg-main-bg min-h-screen">
+        <Services />
+        <Product />
+        <Testimonials />
+        <div className='email-bg pb-4'>
+            <CallAction />
+        </div>
+      </main>
+      <Footer/>
+    </>
   )
 }
